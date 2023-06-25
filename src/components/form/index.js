9d0ee0fd449f14b759cd5c34ceddba59dbd695aa
sasha-valderrama <103856472+sasha-valderrama@users.noreply.github.com>
2023-06-25 22:29:09 +0200

    Refactor Router

---
+++ src/components/form/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { OuterContainer } from './OuterContainer';
+import { MainForm } from './MainForm';
+import Heading from './heading';
+
+export default function Form({ firstName, lastName, jobPosition }) {
+  return (
+    <OuterContainer>
+      <MainForm>
+        <Heading
+          firstName={firstName}
+          lastName={lastName}
+          jobPosition={jobPosition}
+        ></Heading>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+        <div>dfdsfdsfdsfdf</div>
+      </MainForm>
+    </OuterContainer>
+  );
+}
